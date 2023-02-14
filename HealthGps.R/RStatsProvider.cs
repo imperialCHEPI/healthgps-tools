5f7765bba3fa7ea944accc860332f2b810d03e0d
Israel Vieira <i.vieira@imperial.ac.uk>
2023-02-14 10:01:20 +0000

    Updated dependencies order

--- HealthGps.R/RStatsProvider.cs
+++ HealthGps.R/RStatsProvider.cs
@@ -1,9 +1,10 @@
-﻿using RDotNet;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+
+using RDotNet;
 
 namespace HealthGps.R
 {
